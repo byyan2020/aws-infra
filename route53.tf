f2397e0dc2f5df571d5597423428925037e9f6e8
Biyu Yan <yanbiyu2019@outlook.com>
2023-03-17 13:09:37 -0700

    Add route 53

---
+++ route53.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "selected" {
+  name         = "demo.yanbiyu.me"
+  private_zone = false
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = data.aws_route53_zone.selected.name
+  type    = "A"
+  ttl     = 60
+  records = [aws_instance.webapp_instance.public_ip]
+}
